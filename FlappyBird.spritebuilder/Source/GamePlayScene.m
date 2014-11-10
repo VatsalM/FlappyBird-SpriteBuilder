862f895d05e5dc124466475b400d4bd6d44fcd05
Vatsal Mehta <SureshMehta@Vatsals-MBP.cable.rcn.com>
2014-11-10 16:40:12 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -9,10 +9,14 @@
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
 }
-- (void)update:(CCTime)delta
+/*- (void)update:(CCTime)delta
 {
     // this will be run every frame.
     // delta is the time that has elapsed since the last time it was run. This is usually 1/60, but can be bigger if the game slows down
+    }*/
+
+-(void)update:(CCTime)delta
+{
     timeSinceObstacle += delta; // delta is approximately 1/60th of a second
     
     // Check to see if two seconds have passed
@@ -24,12 +28,7 @@
         // Then reset the timer.
         timeSinceObstacle = 0.0f;
     }
-}
 
--(void)update:(CCTime)delta
-{
-    // put update code here
-}
 
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
 {
